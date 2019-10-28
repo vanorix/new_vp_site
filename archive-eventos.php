d9a297f7d49e637d3731d59f5c7cfdf1c66ffcd0
Jose Luis Beltre <lunbel1203@gmail.com>
2019-10-28 08:56:49 -0400

    Estructura de Eventos

---
+++ archive-eventos.php
@@ -0,0 +1,13 @@
+<?php get_header(); ?>
+
+    <div id="eventos-archive">
+        <?php if (have_posts()) : while (have_posts()) : the_post();
+            get_template_part( 'templates/eventos-nacionales');
+        endwhile;
+            // get_template_part('pagination');
+        else : ?>
+            <h3>No se encontraron eventos</h3>
+        <?php endif; ?>
+    </div>
+
+<?php get_footer(); ?>
