95e738286ce6ae482d33459fa92f01b778895dd9
Jose Luis Beltre <lunbel1203@gmail.com>
2019-10-30 12:56:10 -0400

    Primeros estilos
    Agregado de estilos

--- archive-discursos.php
+++ archive-discursos.php
@@ -1,15 +1,20 @@
 <?php get_header(); ?>
-
-    <div id="discursos-archive">
-        <div class="container">
-            <?php if (have_posts()) : while (have_posts()) : the_post();
-                get_template_part( 'templates/loop-discursos');
-            endwhile;
-                // get_template_part('pagination');
-            else : ?>
-                <h3>No se encontraron artículos</h3>
-            <?php endif; ?>
+    <main class="discursos">
+        <div class="hero-title">
+            <h1>Discursos</h1>
         </div>
-    </div>
+
+        <div id="discursos-archive">
+            <div class="container">
+                <?php if (have_posts()) : while (have_posts()) : the_post();
+                    get_template_part( 'templates/loop-discursos');
+                endwhile;
+                    // get_template_part('pagination');
+                else : ?>
+                    <h3>No se encontraron artículos</h3>
+                <?php endif; ?>
+            </div>
+        </div>
+    </main>
 
 <?php get_footer(); ?>
