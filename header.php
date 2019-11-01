ac0fd726e7e8a230b4c76a160515785d10fed9b5
Jose Luis Beltre <lunbel1203@gmail.com>
2019-11-01 15:15:48 -0400

    Actualizado todo

--- header.php
+++ header.php
@@ -47,12 +47,12 @@
             <i class="fas fa-times"></i>
         </div>
         <?php wp_nav_menu( array(
-            'menu'  => 'main-menu'
+            'menu'  => 'main_menu'
         )); ?>
     </nav>
     <!-- MAIN MENU -->
     <nav class="main-menu">
         <?php wp_nav_menu( array(
-            'menu'  => 'main-menu'
+            'menu'  => 'main_menu'
         )); ?>
     </nav>
